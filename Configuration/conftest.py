557292fff21ac25ff33e60f5e15e32cbac1ab1aa
msmfathih <msmfathih@users.noreply.github.com>
2020-11-07 22:12:30 +0400

    edge added

--- Configuration/conftest.py
+++ Configuration/conftest.py
@@ -7,12 +7,14 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
 
-@pytest.fixture(params=["chrome"],scope='class')
+@pytest.fixture(params=["edge","chrome"],scope='class')
 def init_driver(request):
     if  request.param == "chrome":
         web_driver = webdriver.Chrome(ChromeDriverManager().install())
-    if  request.param == "firefox":
+    elif  request.param == "firefox":
         web_driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    else:
+        web_driver = webdriver.Edge(EdgeChromiumDriverManager().install())
 
     request.cls.driver = web_driver
     yield
